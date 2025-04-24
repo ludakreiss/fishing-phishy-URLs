b5264637d0cca3acde33f2e2301df66bd0525e2e
ludakriess <hendsaid2004@gmail.com>
2025-04-24 12:24:53 +0200

    uploaded new scalaer objects for each mode.l, added new imports

--- fishing-phishy-URLs.py
+++ fishing-phishy-URLs.py
@@ -7,6 +7,10 @@ from tensorflow import keras
 from keras import models
 import feature_extractor
 from sklearn.preprocessing import StandardScaler
+import xgboost as xgb
+from tensorflow.keras.utils import get_custom_objects
+from tensorflow.keras.models import load_model
+from tcn import TCN 
 
 st.set_page_config(
     page_title = "Phsihing Detection System",
@@ -33,30 +37,71 @@ model = st.selectbox("Pick the model",
 
 url = st.text_input(label = "Enter the URL")
 
-joblib.load("Models/Datatset #2/ TCN/scaler.joblib")
+scaler = joblib.load("Models/Dataset #2/TCN/scaler.joblib")
 
-url_df = feature_extractor.extract_features(url)
-# url_scaled = scaler.transform(url_df) 
+get_custom_objects().update({'TCN': TCN})
 
-if model == "Linear SVC":
-    svc = joblib.load("Models/Dataset #2/Linear SVC/LinearSVC #2.joblib")
-    prediction = svc.predict(url_df)
+if url:
+    url_df = feature_extractor.extract_features(url)
+    
+    
 
-elif model == "Multi-layer Perceptron":
-    mlp = joblib.load("Models/Dataset #2/MLP/MLP #2.joblib")
-    prediction = mlp.predict(url_df)
+    # if model == "Temporal Convolutional Network":
+    #     url_input = np.array(url_scaled).reshape(url_scaled.shape[0], url_scaled.shape[1], 1)
+    # else:
+    #     url_input = pd.DataFrame(url_scaled, columns=url_df.columns)
 
-elif model == "Random Forest":
-    rf = joblib.load("Models/Dataset #2/Random Forest/rand_forest #2.joblib")
-    prediction = rf.predict(url_df)
+    if model == "Linear SVC":
+        svc = joblib.load("Models/Dataset #2/Linear SVC/LinearSVC #2.joblib")
+        scaler = joblib.load("Models/Dataset #2/Linear SVC/scaler.joblib")
 
-elif model == "Temporal Convolutional Network":
-    tcn = models.load_model("Models/Dataset #2/TCN/TCN #2.h5")
-    url_tcn = np.array(url_df).reshape(url_df.shape[0], url_df.shape[1], 1) 
-    prediction = tcn.predict(url_tcn)
+        url_scaled = scaler.transform(url_df) 
 
-elif model == "XGBoost":
-    xgb = joblib.load("Models/Dataset #2/XGBoost/XGBoost #2.joblib")
-    prediction = xgb.predict(url_df)
+        prediction = svc.predict(url_scaled)
 
-st.text(f"Your url is {prediction}% phishy")
+        label = prediction[0]
+        st.text(f"Your URL {'smells phishy' if label == 1 else 'does not smell phishy'}")
+
+    elif model == "Multi-layer Perceptron":
+        mlp = joblib.load("Models/Dataset #2/MLP/MLP #2.joblib")
+        scaler = joblib.load("Models/Dataset #2/MLP/scaler.joblib")
+
+        url_scaled = scaler.transform(url_df) 
+
+        prediction = mlp.predict(url_scaled)
+
+        label = prediction[0]
+        st.text(f"Your URL {'smells phishy' if label == 1 else 'does not smell phishy'}")
+
+    elif model == "Random Forest":
+        rf = joblib.load("Models/Dataset #2/Random Forest/rand_forest #2.joblib")
+
+        prediction = rf.predict(url_df)
+
+        st.write("Extracted Features:", url_df)
+        st.write("Prediction:", prediction)
+
+        label = prediction[0]
+
+        st.text(f"Your url is {prediction}% phishy")
+
+    elif model == "Temporal Convolutional Network":
+        tcn = models.load_model("Models/Dataset #2/TCN/TCN #2.h5")
+        scaler = joblib.load("Models/Dataset #2/TCN/scaler.joblib")
+        url_scaled = scaler.transform(url_df) 
+        url_tcn = np.array(url_scaled).reshape(url_scaled.shape[0], url_scaled.shape[1], 1) 
+        prediction = tcn.predict(url_tcn)
+        st.text(f"Your url is {prediction}% phishy")
+
+    elif model == "XGBoost":
+        xgb = joblib.load("Models/Dataset #2/XGBoost/XGBoost #2.joblib")
+
+        prediction = xgb.predict(url_df)
+        st.write("Extracted Features:", url_df)
+        st.write("Prediction:", prediction)
+
+        label = prediction[0]
+
+        st.text(f"Your URL is {'phishy' if prediction[0] == 1 else 'safe'}")
+
+
